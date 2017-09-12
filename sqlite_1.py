d09a8fd40611dbef082b76d2e6de067948f891dd
Edit <zyq_victory@163.com>
2017-09-12 16:29:14 +0800

    Update sqlite_1.py

--- sqlite_1.py
+++ sqlite_1.py
@@ -5,25 +5,25 @@ end = datetime.date.today()
 maotai2 = web.DataReader("600519.SS", "yahoo", start, end)
 # for i in range(4, 8):
 #     print list(maotai2.iloc[i,:])
-# next
+
 
 import sqlite3
-conn = sqlite3.connect('D:/sqlite3/maotai02.db')#(":memory:")
+conn = sqlite3.connect('D:/sqlite3/maotai02.db')  #(":memory:")
 c = conn.cursor()
-#新建数据库
+#
 #c.execute('''create table ceshi(user text, note text)''')
-#增
+#
 #c.execute('''insert into ceshi(user,note) values('11','21')''')
-#改
+#
 #c.execute('''update ceshi(user, note) set note = '22' where user = '11' ''')
-#删
+#
 
 #
 #c.execute('''create table maotai122201(open, high, low, close, volume, adjclose)''')
-for i in range(1, len(maotai2 + 1)):#[1,len(maotai2) + 1, 1]
+for i in range(1, len(maotai2 + 1)):     #[1,len(maotai2) + 1, 1]
     mt = list(maotai2.iloc[i,:])
     c.execute('''insert into maotai122201 values(?,?,?,?,?,?)''', mt)
-next
+
 conn.commit()
 c.execute('''select * from maotai21''').fetchall()
 rec = c.execute('''select * from maotai21''')
